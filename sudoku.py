233363a5641fd692d6a1d82a6d61f7378e70a6fc
99cardz <jonathan.schulze.de@gmail.com>
2019-08-16 20:41:56 +0200

    added text file input

--- sudoku.py
+++ sudoku.py
@@ -10,13 +10,13 @@ def display_board(board, messange):
 def checkavailable(input_value, h, v):
     #check for horizontal line
     if input_value in sudoku_copy[h]:
-        print ("in h")
+        #print ("in h")
         return 1
     #check for vertical line
     for i in range(0,8):
         #print (i)
         if input_value == sudoku_copy[i][v]:
-            print ("in v")
+            #print ("in v")
             return 1
     #check for block
     threebythree = []
@@ -34,7 +34,7 @@ def checkavailable(input_value, h, v):
     block_number = (c-1)*3+r
     #print(block_number)
     if input_value in threebythree[block_number-1]:
-        print("in block "+str(block_number))
+        #print("in block "+str(block_number))
         return 1
 
 def goback(horizontal,vertical, number):
@@ -46,31 +46,50 @@ def goback(horizontal,vertical, number):
             vertical = 8
         else:
             vertical -= 1
-        print(horizontal, vertical)
-        print(sudoku_copy[horizontal][vertical])
+        #print(horizontal, vertical)
+        #print(sudoku_copy[horizontal][vertical])
         if sudoku[horizontal][vertical] == 0 and sudoku_copy[horizontal][vertical] != 9:
             number = sudoku_copy[horizontal][vertical] +1
-            print(number)
+            #print(number)
             return horizontal, vertical, number
+def read_txt(sudoku):
+    f = open("sudoku.txt")
+    all_lines = f.readlines()
+    all_lines = [line[:-1]for line in all_lines]
+    #print(all_lines)
+    for h in all_lines:
+        line = []
+        for v in h:
+            line.append(int(v))
+        sudoku.append(line)
+    #print(sudoku)
+
+def input_sudoku():
+    for h in range(9):
+        line = []
+        rawline = input("Line "+str(h+1)+":")
+        for v in rawline:
+            line.append(int(v))
+        sudoku.append(line)
+    #print(sudoku)
+
+
+#Start
 
 print("Sudoku Solver with bruteforce method")
 sudoku = []
-for h in range(9):
-    line = []
-    rawline = input("Line "+str(h+1)+":")
-    for v in rawline:
-        line.append(int(v))
-    sudoku.append(line)
-print(sudoku)
 
+read_txt(sudoku)
+#input_sudoku()
 sudoku_copy = deepcopy(sudoku)
+print(sudoku)
 
 display_board(sudoku,"This is your Sudokuboard:")
 
 horizontal = 0
 vertical = 0
 number = 1
-t = 0
+t = 2
 start_time = time.time()
 while 1>0 :
     if sudoku[horizontal][vertical] != 0:
@@ -79,10 +98,10 @@ while 1>0 :
             horizontal += 1
         else:
             vertical += 1
-        print("next position because not available")
+        #print("next position because not available")
     elif number == 10:
         horizontal, vertical, number = goback(horizontal, vertical, number)
-        print("going back to last possble position")
+        #print("going back to last possble position")
     elif not checkavailable(number, horizontal, vertical):
         #print(horizontal, vertical, number)
         sudoku_copy[horizontal][vertical] = number
@@ -97,10 +116,11 @@ while 1>0 :
         number+=1
     if horizontal == 9:
         display_board(sudoku_copy, "This is the solution to your board:")
-        print("it took "+ str(start_time-time.time())+"seconds")
+        display_board(sudoku, "This is the Original Board:")
+        print("it took "+ str(time.time()-start_time)+"seconds")
         break
     #print(horizontal, vertical)
     #display_board(sudoku_copy,"")
-    if t >= 50:
+    if time.time() - start_time>= t:
         display_board(sudoku_copy,"Current Process:")
-        t = 0
+        t += t
